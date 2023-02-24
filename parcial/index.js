032465b0a04fd6473ea2668d3c432610a8af63a1
Nathaly Rodriguez <87873901+Sarai122002@users.noreply.github.com>
2023-02-24 13:00:31 -0600

    Delete parcial directory

--- parcial/index.js
+++
@@ -1,9 +0,0 @@
-function adoptar(nombreMascota) {
-    var nombre = prompt('Ingresa tu nombre:');
-    if (nombre != null) {
-    alert('Gracias ' + nombre + ' por adoptar a ' + nombreMascota);
-    }
-    }
-
-
-   
