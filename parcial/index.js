a849eca7b1b614bf7bd5b2f21e65e38071a31f59
Nathaly Rodriguez <87873901+Sarai122002@users.noreply.github.com>
2023-02-24 12:09:34 -0600

    Add files via upload

---
+++ parcial/index.js
@@ -0,0 +1,9 @@
+function adoptar(nombreMascota) {
+    var nombre = prompt('Ingresa tu nombre:');
+    if (nombre != null) {
+    alert('Gracias ' + nombre + ' por adoptar a ' + nombreMascota);
+    }
+    }
+
+
+   
